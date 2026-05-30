9786387bb0dcbb9197d348143b67c6c3c51a67fc
m sirisha <msirisha454@gmail.com>
2026-05-30 22:52:40 +0530

    voice response fix

--- lakme/frontend/src/components/AIAssistantSection.js
+++ lakme/frontend/src/components/AIAssistantSection.js
@@ -223,8 +223,16 @@ if (serviceMatch && timeMatch && dateMatch) {
       })
     });
     var data = await res.json();
-    var reply = (data && data.choices && data.choices[0] && data.choices[0].message && data.choices[0].message.content)
-      || "I'm sorry, I couldn't understand that. Please try again!";
+
+    console.log("VOICE RESPONSE:", data);
+    console.log("VOICE RESPONSE JSON:", JSON.stringify(data, null, 2));
+
+    var reply = (data &&
+      data.choices &&
+      data.choices[0] &&
+      data.choices[0].message &&
+      data.choices[0].message.content)
+    || "I'm sorry, I couldn't understand that. Please try again!";
     addMessage('assistant', reply);
     speak(reply);
   } catch(e) {
