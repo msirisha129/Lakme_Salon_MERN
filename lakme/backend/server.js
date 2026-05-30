c6f46a6f91c51586b3ac971c083d77427fc19285
m sirisha <msirisha454@gmail.com>
2026-05-30 18:40:05 +0530

    manual cors headers

--- lakme/backend/server.js
+++ lakme/backend/server.js
@@ -11,6 +11,17 @@ dotenv.config();
 const app = express();
 
 // Middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(cors({
   origin: true,
   credentials: true
