7b684480a3bec9ac792cd3e96e4c961d7dcee7e9
m sirisha <msirisha454@gmail.com>
2026-05-30 19:44:37 +0530

    cors

--- lakme/backend/server.js
+++ lakme/backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-const cors = require('cors');
+
 
 app.use(cors({
   origin: [
@@ -20,6 +20,10 @@ app.use(cors({
   ],
   credentials: true
 }));
+app.use((req, res, next) => {
+  console.log("Origin:", req.headers.origin);
+  next();
+});
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
